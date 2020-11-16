b1beaf10e895a0d453fb3ab915d44e30d9617a2a
Alex Anderson <alexanderson1993@gmail.com>
2020-11-16 15:15:37 -0500

    Add a route to display a random dad joke

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import http, { IncomingMessage, ServerResponse } from "http";
 import path from "path";
 import fs from "fs/promises";
 import url from "url";
+import fetch from "node-fetch";
+
+interface Joke {
+  id: string;
+  joke: string;
+}
 
 async function requestListener(req: IncomingMessage, res: ServerResponse) {
   const parsedUrl = url.parse(req.url || "");
@@ -14,6 +20,19 @@ async function requestListener(req: IncomingMessage, res: ServerResponse) {
     data = await fs.readFile(filePath, "utf-8");
   } catch {
     data = await fs.readFile(path.join(__dirname, "static/404.html"), "utf-8");
+  }
+
+  // Special handling of the dad joke path.
+  if (parsedUrl.pathname === "/dad-joke") {
+    const response = await fetch("https://icanhazdadjoke.com", {
+      headers: {
+        accept: "application/json",
+        "user-agent": "NodeJS Server",
+      },
+    });
+    const joke: Joke = await response.json();
+
+    data = data.replace(/{{joke}}/gm, joke.joke);
   }
 
   res.writeHead(200, {
