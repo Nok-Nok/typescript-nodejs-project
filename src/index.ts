37fb8dcb85fe2c52f02b54af4b6f8a87c71463d1
Alex Anderson <alexanderson1993@gmail.com>
2020-11-16 10:04:20 -0500

    Add multiple routes.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import http, { IncomingMessage, ServerResponse } from "http";
 import path from "path";
 import fs from "fs/promises";
+import url from "url";
 
 async function requestListener(req: IncomingMessage, res: ServerResponse) {
-  const filePath = path.join(__dirname, "static/index.html");
+  const parsedUrl = url.parse(req.url || "");
 
-  const data = await fs.readFile(filePath);
+  let data = "";
+  try {
+    let pathName = parsedUrl.pathname;
+    if (pathName === "/") pathName = "/index";
+    const filePath = path.join(__dirname, `static${pathName}.html`);
+    data = await fs.readFile(filePath, "utf-8");
+  } catch {
+    data = await fs.readFile(path.join(__dirname, "static/404.html"), "utf-8");
+  }
 
   res.writeHead(200, {
     "Content-Type": "text/html",
